1395f7600695ed1113b1cc6d27f2abbd2bc813b3
Emanoel Júnior <emanoel.lima@unb.br>
2023-08-09 16:13:31 -0300

    Commit Inicial

---
+++ src/main/java/br/unb/dominio/Disciplina.java
@@ -0,0 +1,98 @@
+package br.unb.dominio;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Disciplina {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private String turma;
+	private String curso;
+	private String local;
+
+	@ManyToMany
+	@JoinTable(name = "aluno_disciplina", joinColumns = @JoinColumn(name = "disciplina_id"), inverseJoinColumns = @JoinColumn(name = "aluno_id"))
+	private List<Aluno> alunos;
+
+	public Disciplina(String nome, String turma, String curso, String local) {
+		super();
+		this.nome = nome;
+		this.turma = turma;
+		this.curso = curso;
+		this.local = local;
+	}
+
+	public Disciplina(int id, String nome, String turma, String curso, String local) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.turma = turma;
+		this.curso = curso;
+		this.local = local;
+	}
+
+	public Disciplina() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTurma() {
+		return turma;
+	}
+
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+
+	
+}
